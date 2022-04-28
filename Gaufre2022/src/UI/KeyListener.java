e1195787479db7d4532613c0b4488670cb33aa6a
NadimBabba <75009254+NadimBabba@users.noreply.github.com>
2022-04-28 18:29:13 +0200

    Ajout de la methode vide keypressed

--- Gaufre2022/src/UI/KeyListener.java
+++ Gaufre2022/src/UI/KeyListener.java
@@ -1,5 +1,26 @@
 package UI;
 
-public class KeyListener {
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyListener extends KeyAdapter {
+	
+	@Override
+	public void keyPressed(KeyEvent event) {
+		switch (event.getKeyCode()) {
+			case KeyEvent.VK_Q:
+				// QUIT
+				break;
+			case KeyEvent.VK_R:
+				// RESET
+				break;
+			case KeyEvent.VK_L:
+				// LOAD
+				break;
+			case KeyEvent.VK_ESCAPE:
+				// FULLSCREEN
+				break;
+		}
+	}
 
 }
