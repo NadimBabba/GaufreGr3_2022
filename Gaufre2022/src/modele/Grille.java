7b9cc88422c4c153e66d0122e328320080ce7062
rodolphe beguin <95235083+yukinonyukinoshita@users.noreply.github.com>
2022-04-28 19:00:45 +0200

    Update Grille.java
    test

--- Gaufre2022/src/modele/Grille.java
+++ Gaufre2022/src/modele/Grille.java
@@ -2,4 +2,17 @@ package modele;
 
 public class Grille {
   int [][]tableau ;
+  int nbcolones ;
+  int nblignes ;
+  int joueurquijoue ;
+  
+  
+  
+  
+  Grille(int c , int l  ){
+	  tableau = new int[c][l];
+	  nbcolones = c ;
+	  nblignes = l ;
+	  joueurquijoue = 1 ;
+  }
 }
