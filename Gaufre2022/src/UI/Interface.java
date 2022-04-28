fbd2c99000d13d125b273f436030ee13857123f6
NadimBabba <75009254+NadimBabba@users.noreply.github.com>
2022-04-28 18:36:02 +0200

    Modification du fichier interface

---
+++ Gaufre2022/src/UI/Interface.java
@@ -0,0 +1,13 @@
+package UI;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Interface extends JComponent {
+	
+	@Override
+    public void paintComponent (Graphics g) {
+		Graphics2D drawable = (Graphics2D) g;
+	}
+
+}
